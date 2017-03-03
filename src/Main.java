a4ee7db7a394d2098cce696d05ac323df9a7f8bb
lvazquez-ditupm <lvazquez-ditupm>
2017-03-03 15:40:03 +0100

    finalizado sensor

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,113 @@
 
 /**
+ * This class executes the "Presencia+Activos+Estado" (PAE) sensor
  *
- * @author root
+ * @author UPM (member of DHARMA Development Team) (http://dharma.inf.um.es)
+ * @version 1.0
  */
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        final int T = Integer.parseInt(args[1]);
         FileReader f = null;
-        try {
-            byte[] encoded = Files.readAllBytes(Paths.get("/home/saturno/NetBeansProjects/SensorPAE/test.json"));
-            //System.out.println(detectAnomalies(new String(encoded, "UTF-8")));
-            addDataToDB(new String(encoded, "UTF-8"));
+        Calendar midnight = Calendar.getInstance();
+        midnight.set(Calendar.HOUR_OF_DAY, 0);
+        midnight.set(Calendar.MINUTE, 0);
+        midnight.set(Calendar.SECOND, 0);
+        midnight.set(Calendar.MILLISECOND, 0);
 
-        } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        Calendar nowCal = Calendar.getInstance();
+        long secondsFromMidnight = 0;
 
+        if (args.length != 5 || (!args[0].equals("exec") && !args[0].equals("train"))) {
+            System.err.println("No se ha introducido un parámentro correcto (exec/train)");
+            System.exit(0);
+        }
+
+        // Espera hasta que el tiempo actual acabe en 0
+        while (true) {
+            nowCal.setTime(new Date());
+            secondsFromMidnight = (nowCal.getTimeInMillis() - midnight.getTimeInMillis()) / 1000;
+
+            if (secondsFromMidnight % 10 == 0) {
+                break;
+            }
+        }
+
+        System.out.println("-- Start PAE sensor --");
+
+        int weekDay;
+        int time;
+
+        while (true) {
+            nowCal.setTime(new Date());
+            secondsFromMidnight = (nowCal.getTimeInMillis() - midnight.getTimeInMillis()) / 1000;
+
+            weekDay = nowCal.get(Calendar.DAY_OF_WEEK);
+            time = (int) (T * (Math.ceil(Math.abs(secondsFromMidnight / T)))); //Redondea a múltiplo de T (segundos)
+
+            try {
+                byte[] encoded = Files.readAllBytes(Paths.get("/home/saturno/NetBeansProjects/SensorPAE/test.json"));
+                if (args[0].equals("exec")) {
+                    System.out.println("Detectando anomalías:");
+                    ArrayList<String> anomalies = detectAnomalies(args[2], args[3], new String(encoded, "UTF-8"), weekDay, time);
+                    processAnomalies(anomalies, secondsFromMidnight, args[4]);
+                } else if (args[0].equals("train")) {
+                    addDataToDB(args[2], args[3], new String(encoded, "UTF-8"), weekDay, time);
+                } else {
+                    throw new Exception("Parámetro incorrecto (exec/train)");
+                }
+                Thread.sleep(900000);
+            } catch (Exception ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                System.exit(0);
+            }
         }
     }
 
-    private static ArrayList<String> detectAnomalies(String path) throws SQLException {
+    /**
+     * Obtiene los valores de estado de las salas y activos, y los compara con
+     * la base de datos para detectar anomalías
+     *
+     * @param path ruta al fichero input
+     * @param weekDay día de la semana (1-domingo, 7-sábado)
+     * @param time segundos desde la medianoche
+     * @return lista con las anomalías detectadas
+     */
+    private static ArrayList<String> detectAnomalies(String user, String passwd, String path, int weekDay, int time) throws SQLException {
 
         ArrayList<LinkedTreeMap<String, Object>> jsonMap = new Gson().fromJson(path, ArrayList.class);
         ArrayList<String> anomalies = new ArrayList<>();
-
-        Date now = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(now);
-        int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
-
-        Calendar fromMidnight = Calendar.getInstance();
-        fromMidnight.set(Calendar.HOUR, 0);
-        fromMidnight.set(Calendar.MINUTE, 0);
-        fromMidnight.set(Calendar.SECOND, 0);
-        fromMidnight.set(Calendar.MILLISECOND, 0);
-
-        long secondsFromMidnight = (calendar.getTimeInMillis() - fromMidnight.getTimeInMillis()) / 1000;
-        int time = (int) (900 * (Math.ceil(Math.abs(secondsFromMidnight / 900))));
 
         for (LinkedTreeMap<String, Object> roomActive : jsonMap) {
             int roomID = (int) (double) roomActive.get("SALA");
             int roomStatus = (int) (double) roomActive.get("ESTADO");
             LinkedTreeMap<String, Double> activesStatus = new LinkedTreeMap<>();
-            try {
-                ArrayList<LinkedTreeMap<String, Double>> activesList = (ArrayList<LinkedTreeMap<String, Double>>) roomActive.get("ACTIVOS");
-                for (LinkedTreeMap<String, Double> active : activesList) {
-                    activesStatus.putAll(active);
-                }
-            } catch (ClassCastException e) {
-                activesStatus.putAll((LinkedTreeMap<String, Double>) roomActive.get("ACTIVOS"));
-            }
+            activesStatus.putAll((LinkedTreeMap<String, Double>) roomActive.get("ACTIVOS"));
 
-            int roomStatusDB = MySQL.getRoomStatus(roomID, weekDay, time);
-            HashMap<String, Integer> activesStatusDB = MySQL.getActivesStatusPerRoom(roomID, weekDay, time);
+            int roomStatusDB = MySQL.getRoomStatus(user, passwd, roomID, weekDay, time);
+            HashMap<String, Integer> activesStatusDB = MySQL.getActivesStatusPerRoom(user, passwd, roomID, weekDay, time);
 
             if (roomStatusDB == -1 || activesStatusDB.isEmpty()) {
                 throw new SQLException("No se han encontrado datos en la base de datos");
             }
 
             if (roomStatus != roomStatusDB) {
-                anomalies.add("La sala \"" + roomID + "\" tiene un ocupación anómala: " + roomStatus);
+                anomalies.add("La sala \"" + roomID + "\" tiene una ocupación anómala: " + roomStatus);
             }
 
             int diff = activesStatusDB.size() - activesStatus.size();
@@ -89,58 +116,54 @@ public class Main {
                 for (String active : activesStatusDB.keySet()) {
                     if (!activesStatus.containsKey(active)) {
                         anomalies.add("Activo \"" + active + "\" no encontrado en la sala " + roomID);
+                    } else if (!activesStatusDB.containsKey(active)) {
+                        anomalies.add("Activo \"" + active + "\" detectado en la sala " + roomID);
+
                     } else if (activesStatus.get(active).intValue() != activesStatusDB.get(active)) {
-                        anomalies.add("Activo \"" + active + "\" con valor anómalo");
-                    }
-                }
-            } else if (diff < 0) {
-                for (String active : activesStatus.keySet()) {
-                    if (!activesStatusDB.containsKey(active)) {
-                        anomalies.add("Activo \"" + active + "\" no presente en la base de datos");
-                    } else if (activesStatus.get(active).intValue() != activesStatusDB.get(active)) {
-                        anomalies.add("Activo \"" + active + "\" con valor anómalo");
+                        anomalies.add("Activo \"" + active + "\" con valor anómalo: "
+                                + activesStatus.get(active).intValue() + "/" + activesStatusDB.get(active));
                     }
                 }
             } else {
                 for (String active : activesStatus.keySet()) {
-                    if (!activesStatusDB.containsKey(active)) {
-                        anomalies.add("Activo \"" + active + "\" no presente en la base de datos");
+                    if (!activesStatus.containsKey(active)) {
+                        anomalies.add("Activo \"" + active + "\" no encontrado en la sala " + roomID);
+                    } else if (!activesStatusDB.containsKey(active)) {
+                        anomalies.add("Activo \"" + active + "\" detectado en la sala " + roomID);
+
                     } else if (activesStatus.get(active).intValue() != activesStatusDB.get(active)) {
-                        anomalies.add("Activo \"" + active + "\" con valor anómalo: " + activesStatus.get(active).intValue());
+                        anomalies.add("Activo \"" + active + "\" con valor anómalo: "
+                                + activesStatus.get(active).intValue() + "/" + activesStatusDB.get(active));
                     }
                 }
             }
+
+            activesStatus.clear();
         }
 
         return anomalies;
     }
 
-    private static void addDataToDB(String path) throws SQLException {
+    /**
+     * Añade los valores de estado de salas y activos a la base de datos
+     *
+     * @param path ruta al fichero input
+     * @param weekDay día de la semana (1-domingo, 7-sábado)
+     * @param time segundos desde la medianoche
+     */
+    private static void addDataToDB(String user, String passwd, String path, int weekDay, int time) throws Exception {
         ArrayList<LinkedTreeMap<String, Object>> jsonMap = new Gson().fromJson(path, ArrayList.class);
         HashMap<Integer, Integer> roomData = new HashMap<>();
         HashMap<String, Integer> activeStatus = new HashMap<>();
         HashMap<String, Integer> activeLocation = new HashMap<>();
         LinkedTreeMap<String, Double> activesStatus = new LinkedTreeMap<>();
 
-        Date now = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(now);
-        int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
-
-        Calendar fromMidnight = Calendar.getInstance();
-        fromMidnight.set(Calendar.HOUR, 0);
-        fromMidnight.set(Calendar.MINUTE, 0);
-        fromMidnight.set(Calendar.SECOND, 0);
-        fromMidnight.set(Calendar.MILLISECOND, 0);
-
-        long secondsFromMidnight = (calendar.getTimeInMillis() - fromMidnight.getTimeInMillis()) / 1000;
-        int time = (int) (900 * (Math.ceil(Math.abs(secondsFromMidnight / 900))));
-
         for (LinkedTreeMap<String, Object> roomActive : jsonMap) {
             int roomID = (int) (double) roomActive.get("SALA");
             int roomStatus = (int) (double) roomActive.get("ESTADO");
             try {
-                ArrayList<LinkedTreeMap<String, Double>> activesList = (ArrayList<LinkedTreeMap<String, Double>>) roomActive.get("ACTIVOS");
+                ArrayList<LinkedTreeMap<String, Double>> activesList
+                        = (ArrayList<LinkedTreeMap<String, Double>>) roomActive.get("ACTIVOS");
                 for (LinkedTreeMap<String, Double> active : activesList) {
                     activesStatus.putAll(active);
                 }
@@ -154,7 +177,98 @@ public class Main {
             }
             activesStatus.clear();
         }
-        MySQL.addRoomsStatus(roomData, weekDay, time);
-        MySQL.addActivesStatus(activeStatus, activeLocation, time, time);
+        MySQL.addRoomsStatus(user, passwd, roomData, weekDay, time);
+        MySQL.addActivesStatus(user, passwd, activeStatus, activeLocation, weekDay, time);
+        System.out.println("Añadidos datos a la base de datos");
+    }
+
+    /**
+     * Clasifica las anomalías detectadas
+     *
+     * @param anomalies lista de anomalías
+     * @param secondsFromMidnight segundos desde la medianoche
+     * @param path ruta al fichero output
+     */
+    private static void processAnomalies(ArrayList<String> anomalies, long secondsFromMidnight, String path) {
+
+        Gson gson = new Gson();
+        HashMap<String, Object> json = new HashMap<>();
+        HashMap<String, String> roomOcupationAnomalies = new HashMap<>();
+        HashMap<String, String[]> activeUsageAnomalies = new HashMap<>();
+        HashMap<String, String[]> activeRegistrationAnomalies = new HashMap<>();
+
+        HashMap<String, String> isInRoom = new HashMap<>();
+        HashMap<String, String> isNotInRoom = new HashMap<>();
+
+        for (String anomaly : anomalies) {
+
+            System.out.println(anomaly);
+
+            if (anomaly.contains("tiene una ocupación anómala")) {
+                anomaly = anomaly.substring(anomaly.indexOf("\"") + 1);
+                String room = anomaly.substring(0, anomaly.indexOf("\""));
+                String anomalyStatus = anomaly.substring(anomaly.length() - 1);
+                roomOcupationAnomalies.put(room, anomalyStatus);
+
+            } else if (anomaly.contains("con valor anómalo")) {
+                anomaly = anomaly.substring(anomaly.indexOf("\"") + 1);
+                String active = anomaly.substring(0, anomaly.indexOf("\""));
+                String anomalyStatusStr = anomaly.substring(anomaly.length() - 3);
+                String[] anomalyStatus = anomalyStatusStr.split("/");
+                activeUsageAnomalies.put(active, anomalyStatus);
+
+            } else if (anomaly.contains("detectado en la sala")) {
+                anomaly = anomaly.substring(anomaly.indexOf("\"") + 1);
+                String active = anomaly.substring(0, anomaly.indexOf("\""));
+                String room = anomaly.substring(anomaly.indexOf("sala ") + 5);
+                isInRoom.put(active, room);
+
+            } else if (anomaly.contains("no encontrado en la sala")) {
+                anomaly = anomaly.substring(anomaly.indexOf("\"") + 1);
+                String active = anomaly.substring(0, anomaly.indexOf("\""));
+                String room = anomaly.substring(anomaly.indexOf("sala ") + 5);
+                isNotInRoom.put(active, room);
+            }
+
+            if (!isInRoom.isEmpty() && !isNotInRoom.isEmpty()) {
+                ArrayList<String> delete = new ArrayList<>();
+                for (String active : isInRoom.keySet()) {
+                    if (isNotInRoom.containsKey(active)) {
+                        String[] movement = new String[2];
+                        movement[0] = isNotInRoom.get(active);
+                        movement[1] = isInRoom.get(active);
+                        activeRegistrationAnomalies.put(active, movement);
+                        delete.add(active);
+                    }
+                }
+                for (String deleteItem : delete) {
+                    isNotInRoom.remove(deleteItem);
+                    isInRoom.remove(deleteItem);
+                }
+                delete.clear();
+            }
+        }
+
+        json.put("OcupationAnom", (HashMap<String, Object>) (Object) roomOcupationAnomalies);
+        json.put("UsageAnom", (HashMap<String, Object>) (Object) activeUsageAnomalies);
+        json.put("RegistrationAnom", (HashMap<String, Object>) (Object) activeRegistrationAnomalies);
+        json.put("Time", secondsFromMidnight);
+
+        if (roomOcupationAnomalies.isEmpty() && activeRegistrationAnomalies.isEmpty()
+                && activeRegistrationAnomalies.isEmpty()) {
+            return;
+        }
+
+        String jsonString = gson.toJson(json);
+
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(path + "/anomalies.json", "UTF-8");
+            writer.println(jsonString);
+            writer.close();
+
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/MySQL.java
+++ src/MySQL.java
@@ -8,30 +8,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * This class acts as DAO for the MySQL database
  *
- * @author root
+ * @author UPM (member of DHARMA Development Team) (http://dharma.inf.um.es)
+ * @version 1.0
  */
 public class MySQL {
 
-    private static Connection getConToDB() throws SQLException {
+    /**
+     * Establece una conexión con la base de datos
+     *
+     * @param user usuario
+     * @param password contraseña
+     */
+    private static Connection getConToDB(String user, String password) throws SQLException {
         String dbURL = "jdbc:mysql://localhost:3306/PLAsensor"
                 + "?verifyServerCertificate=false"
                 + "&useSSL=false"
                 + "&requireSSL=false";
 
-        return DriverManager.getConnection(dbURL, "root", "asdf");
+        return DriverManager.getConnection(dbURL, user, password);
     }
 
-    public static int getRoomStatus(int roomID, int dayWeek, int time) {
+    /**
+     * Recupera el estado de la sala en un momento dado
+     *
+     * @param user usuario DB
+     * @param passwd contraseña DB
+     * @param roomID identificador de la sala
+     * @param dayWeek día de la semana
+     * @param time segundos desde medianoche
+     * @return estado de la sala (1/0)
+     */
+    public static int getRoomStatus(String user, String passwd, int roomID, int dayWeek, int time) {
         int res = -1;
         try {
-            Connection con = getConToDB();
+            Connection con = getConToDB(user, passwd);
             PreparedStatement stmt = con.prepareStatement(
                     "SELECT roomStatus FROM Room WHERE "
-                    + " roomId=14"
-                    + " and day_week=1"
-                    + " and time=180"
-            //+ "roomId=" + roomID
+                    + "roomId=" + roomID
+                    + " and day_week=3"
+                    + " and time=45000"
             //+ " and day_week=" + dayWeek
             //+ " and time=" + time
             );
@@ -51,16 +68,25 @@ public class MySQL {
         return res;
     }
 
-    public static HashMap<String, Integer> getActivesStatusPerRoom(int roomID, int dayWeek, int time) {
+    /**
+     * Recupera el estado de los activos de una sala
+     *
+     * @param user usuario DB
+     * @param passwd contraseña DB
+     * @param roomID identificador de la sala
+     * @param dayWeek día de la semana
+     * @param time segundos desde medianoche
+     * @return mapa con los activos y sus estados (0-3)
+     */
+    public static HashMap<String, Integer> getActivesStatusPerRoom(String user, String passwd, int roomID, int dayWeek, int time) {
         HashMap<String, Integer> res = new HashMap<>();
         try {
-            Connection con = getConToDB();
+            Connection con = getConToDB(user, passwd);
             PreparedStatement stmt = con.prepareStatement(
                     "SELECT activeID, activeStatus FROM Active WHERE "
-                    + " roomId=14"
-                    + " and day_week= 1"
-                    + " and time=180"
-            //+ "roomId=" + roomID
+                    + "roomId=" + roomID
+                    + " and day_week= 3"
+                    + " and time=45000"
             //+ " and day_week=" + dayWeek
             //+ " and time=" + time
             );
@@ -79,9 +105,18 @@ public class MySQL {
         return res;
     }
 
-    public static void addRoomsStatus(HashMap<Integer, Integer> roomIDStatus, int dayWeek, int time) {
+    /**
+     * Registra en la base de datos los estados de las salas
+     *
+     * @param user usuario DB
+     * @param passwd contraseña DB
+     * @param roomIDStatus mapa con las salas y sus estados (0/1)
+     * @param dayWeek día de la semana
+     * @param time segundos desde medianoche
+     */
+    public static void addRoomsStatus(String user, String passwd, HashMap<Integer, Integer> roomIDStatus, int dayWeek, int time) {
         try {
-            Connection con = getConToDB();
+            Connection con = getConToDB(user, passwd);
 
             String query = "INSERT INTO Room(roomId, day_week, time, roomStatus) VALUES ";
 
@@ -103,14 +138,26 @@ public class MySQL {
         }
     }
 
-    public static void addActivesStatus(HashMap<String, Integer> activeIDStatus,
-            HashMap<String, Integer> activeIDLocation, int dayWeek, int time) {
+    /**
+     * Registra en la base de datos los estados y localizaciones de los activos
+     * @param user usuario DB
+     * @param passwd contraseña DB
+     * @param activeIDStatus mapa con los activos y sus estados (0-3)
+     * @param activeIDLocation mapa con los activos y las salas en las que están
+     * @param dayWeek día de la semana
+     * @param time segundos desde medianoche
+     */
+    public static void addActivesStatus(String user, String passwd, HashMap<String, Integer> activeIDStatus,
+            HashMap<String, Integer> activeIDLocation, int dayWeek, int time) throws Exception {
         try {
-            Connection con = getConToDB();
+            Connection con = getConToDB(user, passwd);
 
             String query = "INSERT INTO Active(activeId, activeStatus, roomID, day_week, time) VALUES ";
 
             for (Map.Entry<String, Integer> entry : activeIDStatus.entrySet()) {
+                if (entry.getValue() < 0 || entry.getValue() > 3) {
+                    throw new Exception("Estado en activo " + entry.getKey() + " no válido");
+                }
                 query += "(\"" + entry.getKey() + "\"," + entry.getValue() + ","
                         + activeIDLocation.get(entry.getKey()) + "," + dayWeek + "," + time + "),";
             }
